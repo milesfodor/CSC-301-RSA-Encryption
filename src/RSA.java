fa6e43d4e13a26251d0309a527868205e0c9db14
agondell4 <agondell4@gmail.com>
2025-12-05 13:16:12 -0500

    Update RSA.java

--- src/RSA.java
+++ src/RSA.java
@@ -4,12 +4,14 @@ import java.util.Base64;
 
 public class RSA {
 
+    //generates key pairs
     public static KeyPair generateKeyPair() throws Exception {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(2048);
         return generator.generateKeyPair();
     }
 
+    //encrypts the message using the public key
     public static String encrypt(String text, PublicKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, key);
@@ -17,6 +19,7 @@ public class RSA {
         return Base64.getEncoder().encodeToString(bytes);
     }
 
+    //decrypts the message using the private key
     public static String decrypt(String text, PrivateKey key) throws Exception {
         byte[] bytes = Base64.getDecoder().decode(text);
         Cipher cipher = Cipher.getInstance("RSA");
@@ -24,10 +27,12 @@ public class RSA {
         return new String(cipher.doFinal(bytes));
     }
 
-    public static String keyToString(PublicKey key) {
+    //converts the key to a string
+    public static String keyToString(PublicKey key) throws Exception {
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 
+    //converts a string to a key
     public static PublicKey stringToKey(String key) throws Exception {
         byte[] bytes = Base64.getDecoder().decode(key);
         KeyFactory factory = KeyFactory.getInstance("RSA");
